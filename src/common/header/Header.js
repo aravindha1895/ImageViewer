205eed1dd7190957d5058cc994d3f29458c956c7
aravindha1895 <sridharanrvnd@gmail.com>
2020-01-04 23:32:44 +0530

    Conversion of header to functional component to support drpdown

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -8,40 +8,65 @@ import OutlinedInput from '@material-ui/core/OutlinedInput';
 import Visibility from '@material-ui/icons/Visibility';
 import { Input } from '@material-ui/core';
 import FormControl from '@material-ui/core/FormControl';
+import Avatar from '@material-ui/core/Avatar';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
-class Header extends Component {
+// class Header extends Component {
 
-    constructor() {
+  
+   /* constructor() {
         super();
     }
+*/
 
-
-
-    render() {
+const Header = function (props){
+    const [anchorEl, setAnchorEl] = React.useState(null);
+  
+    const handleClick = event => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+   // render() {   
         return (
             <div>
                 <header className="app-header" >
                     <p id="appName"> Image Viewer </p>
                     {"true" === "true" ?
                         <span className="home-group" >
-                           
-                                <FilledInput id="outlined-basic"  placeholder="Search..." variant="outlined"
-                                   onChange={(e)=>this.props.onSearchTextChanged(e)}
-                                        startAdornment = {(
-                                            <InputAdornment variant="standard" position="start" id="searchBoxIcon" >
-                                                <SearchOutlinedIcon />
-                                                
-                                        </InputAdornment>
-                                        )}
-                                    
-                                />
-                            
+
+                            <FilledInput id="outlined-basic" placeholder="Search..." variant="outlined"
+                                onChange={(e) => props.onSearchTextChanged(e)}
+                                startAdornment={(
+                                    <InputAdornment variant="standard" position="start" id="searchBoxIcon" >
+                                        <SearchOutlinedIcon />
+
+                                    </InputAdornment>
+                                )}
+
+                            />
+                            <Avatar aria-label="recipe" style={{ float: "right", marginLeft: "10px", cursor: "pointer" }} onClick={handleClick} >
+                                A
+                                </Avatar>
+                            <Menu
+                                id="simple-menu"
+                                anchorEl={anchorEl}
+                                keepMounted
+                                open={Boolean(anchorEl)}
+                                
+                            >
+                                <MenuItem onClick={handleClose}>My account</MenuItem>
+                                <MenuItem onClick={handleClose}>Logout</MenuItem>
+                            </Menu>
                         </span>
                         : ""}
                 </header>
             </div>
         )
-    }
+   // }
 }
 
 export default Header;
