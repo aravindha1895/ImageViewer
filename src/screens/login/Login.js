7212a1468a661c7f0f552cc62647c176f56fdc5d
aravindha1895 <sridharanrvnd@gmail.com>
2020-01-04 11:31:04 +0530

    Login component  initial stub

---
+++ src/screens/login/Login.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Header from '../../common/header/Header';
+import './Login.css';
+
+
+class Login extends Component {
+
+    constructor() {
+        super();
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                Login component
+            </div>
+        )
+    }
+}
+
+export default Login;
